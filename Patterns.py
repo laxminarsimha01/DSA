e7e3e657bbb19af8973bc1ef9e1b4f5c4ab4e87b
laxminarsimha01 <laxminarsimha.nalla@outlook.com>
2026-01-02 23:23:35 +0530

    Updated with new patterns

--- Patterns.py
+++ Patterns.py
@@ -33,18 +33,25 @@ def pattern6(n):
 def pattern7(n):
     for i in range(n):
         print(' '*(n-i+1)+'*'*(2*i+1))
-
 def pattern8(n):
     for i in range(n):
         print(' '*(i)+'*'*((2*n-1)-2*i))
-
-
+def pattern9(n):
+    for i in range(n):
+        print(' '*(n-i-1)+'*'*(2*i+1))
+    for j in range(n):
+        print(' '*j+'*'*(2*(n-j)-1))
+def pattern10(n):
+    for i in range(n):
+        print('*'*(i+1))
+    for j in range(n-1):
+        print('*'*(n-j-1))
 
 def __main__():
      t =  int(input())
      for i in range(t):
          n = int(input())
-         pattern8(n)
+         pattern10(n)
 
 if __name__ == "__main__":
     __main__()
