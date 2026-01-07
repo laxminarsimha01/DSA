10046fc5d3bdf996beb929e8cb19ec10ca4d4fdd
laxminarsimha01 <laxminarsimha.nalla@outlook.com>
2026-01-07 23:15:30 +0530

    New crown pattern added to it

--- Patterns.py
+++ Patterns.py
@@ -105,6 +105,21 @@ def pattern19(n):
         for j in range(i-2,-1,-1):  # Right
             print(chr(65+j), end=' ')
         print()
+def pattern20(n):
+    for i in range(1,n):
+        for j in range(1, n-i+2):
+            print(j, end=' ')
+        print('  '*2*(i-1), end= ' ')
+        for x in range(n-i+1,0,-1):
+            print(x, end=' ')
+        print()
+    for i in range(1, n+1):
+        for j in range(1, i+1):
+            print(j, end=' ')
+        print('  '*2*(n-i), end=' ')
+        for x in range(i,0,-1):
+            print(x, end=' ')
+        print()
 
 
 def __main__():
